798242e0901f04ac75e8abec3140c70ec9983eea
woochangmin <rebuilding33@gmail.com>
2023-04-17 17:46:20 +0900

    feat:몬스터생성

---
+++ Gitreview/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitreview
+{
+    internal class Monster
+    {
+        private int hp;
+        private int mp;
+        private string name;
+
+    }
+}
