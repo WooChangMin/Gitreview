43203335dfef47f74b4736f14b018250843d2245
woochangmin <rebuilding33@gmail.com>
2023-04-17 17:52:53 +0900

    feat : 슬라임기본구현

--- Gitreview/Monster.cs
+++ Gitreview/Monster.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,19 @@ namespace Gitreview
     {
         private int hp;
         private int mp;
-        private string name;
+        protected string name;
 
+        public void TakeDamage(int damage)
+        {
+            Console.WriteLine($"{name}이가 데미지를 {damage}만큼 받습니다.");
+        }
+
+        public class Slime : Monster
+        {
+            public Slime()
+            {
+                this.name = "슬라임";
+            }
+        }
     }
 }
